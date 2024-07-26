8eb24df83431cb9718ba6f4180cbd119d95bfc25
Will Foody <will.foody@calendly.com>
2024-07-25 23:44:13 -0400

    Add title to music iframe

--- src/components/Music.js
+++ src/components/Music.js
@@ -8,7 +8,7 @@ function Music() {
       <Sidebar />
       <div className="musicContainer">
         <div className="musicTitle">music</div>
-        <iframe className="musicPlayer" width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/728970993&color=%23ef40ff&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+        <iframe title='Soundcloud player' className="musicPlayer" width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/728970993&color=%23ef40ff&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
       </div>
     </div>
   );
